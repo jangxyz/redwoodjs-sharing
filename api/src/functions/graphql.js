bc420e7b168eaf1dc0a5cbcb40d83b36215934bd
Kim, Jang-hwan <janghwan@gmail.com>
2021-02-09 14:14:44 +0900

    ADD value1 from shared_js

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -8,6 +8,10 @@ import schemas from 'src/graphql/**/*.{js,ts}'
 import { db } from 'src/lib/db'
 import services from 'src/services/**/*.{js,ts}'
 
+import { value } from '../../../sharing/shared_js'
+
+console.log('🚀 ~ file: graphql.js ~ line 15 ~', { value })
+
 export const handler = createGraphQLHandler({
   schema: makeMergedSchema({
     schemas,
