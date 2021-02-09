78495be858a6d1d62f7b8c266a76574fb4852a77
Kim, Jang-hwan <janghwan@gmail.com>
2021-02-09 14:15:33 +0900

    ADD value2 from shared_export.js
    succeeds on web
    FAILS on api

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -9,8 +9,10 @@ import { db } from 'src/lib/db'
 import services from 'src/services/**/*.{js,ts}'
 
 import { value } from '../../../sharing/shared_js'
+import { value2 } from '../../../sharing/shared_export'
 
 console.log('🚀 ~ file: graphql.js ~ line 15 ~', { value })
+console.log('🚀 ~ file: graphql.js ~ line 16 ~', { value2 })
 
 export const handler = createGraphQLHandler({
   schema: makeMergedSchema({
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,12 +1,14 @@
 import { Link, routes } from '@redwoodjs/router'
 
 import { value } from 'src/../../sharing/shared_js'
+import { value2 } from 'src/../../sharing/shared_export'
 
 const HomePage = () => {
   return (
     <>
       <h1>HomePage</h1>
       <p>sharing value: {value}</p>
+      <p>sharing value2: {value2}</p>
       <p>
         My default route is named <code>home</code>, link to me with `
         <Link to={routes.home()}>Home</Link>`
